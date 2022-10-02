98355f16a9fadcc2a35571996952daadffbc6c4f
maulanabin <2041720132@student.polinema.ac.id>
2022-10-02 09:29:32 +0700

    Menambahkan migrations

--- database/migrations/2022_09_30_012536_create_users_table.php
+++ database/migrations/2022_09_30_012536_create_users_table.php
@@ -16,11 +16,19 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('level');
+            $table->string('fotoprofil', 255);
             $table->string('email')->unique();
+            $table->string('phone', 13);
+            $table->text('alamat');
+            $table->unsignedInteger('provinsi');
+            $table->unsignedInteger('kota');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
+            // $table->foreign('provinsi')->references('province_id')->on('provinces')->onUpdate('cascade')->onDelete('restrict');
+            // $table->foreign('kota')->references('city_id')->on('cities')->onUpdate('cascade')->onDelete('restrict');
         });
     }
 
